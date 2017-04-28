b25090c6ab94cd1769542414b47f2308c2d1cd3d
mongo74 <benoitgevaert@gmail.com>
2017-04-28 10:12:03 +0200

    packages update

--- website/Global.asax.cs
+++ website/Global.asax.cs
@@ -13,7 +13,7 @@ namespace bvlf_v2
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
